736302d26c71197abf3e92fed97bdc66f179e9e4
gooruru <gooruru@hanmail.net>
2021-02-03 14:08:53 +0900

    초기 커밋.

---
+++ GitHub/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitHub
+{
+    public partial class Form1 : Form
+    {
+        int test = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
